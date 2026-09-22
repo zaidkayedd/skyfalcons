c3da7843af9fba6fb188bc7fb9a4127da769d52f
ZaidAlKayed <zaidkayed926@gmail.com>
2026-09-22 15:37:29 +0300

    Update

--- components/LogoLoader.tsx
+++ components/LogoLoader.tsx
@@ -10,16 +10,17 @@ export function LogoLoader({ isLoading }: LogoLoaderProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#1f1f1f] backdrop-blur-sm transition-opacity">
       <div className="flex flex-col items-center gap-4">
-        <div className="relative h-30 w-30">
+      
+        <div className="relative h-32 w-32">
           <Image
             src="/loader.gif"
             alt="Loading..."
             fill
+            unoptimized 
             className="object-contain"
             priority
           />
         </div>
-
       </div>
     </div>
   );
